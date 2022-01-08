43cc82aa0d81ca4aee40907fa5729fe34f6f9ddb
Joao Marcelo de Jesus Macedo <joaomacedo.millenium@poupex.com.br>
2022-01-08 17:54:25 -0300

    Projeto criado (Swagger ja adicionado)

---
+++ src/main/java/com/pedidohamburguer/PedidoHamburguerApplication.java
@@ -0,0 +1,13 @@
+package com.pedidohamburguer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoHamburguerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoHamburguerApplication.class, args);
+	}
+
+}
